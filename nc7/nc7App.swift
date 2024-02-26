b7eab6e9a66fe6f3576a2c2e7c84bd72d733fbf8
Pedro Pessuto <pedro.pessuto@outlook.com>
2024-02-26 14:59:31 -0300

    Initial Commit

---
+++ nc7/nc7App.swift
@@ -0,0 +1,17 @@
+//
+//  nc7App.swift
+//  nc7
+//
+//  Created by Pedro Pessuto on 26/02/24.
+//
+
+import SwiftUI
+
+@main
+struct nc7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
