1e40605cf6abfa6f28ae871c2d01f9859aed5f72
Pedro Pessuto <pedro.pessuto@outlook.com>
2024-02-28 14:52:16 -0300

    done

--- nc7/ContentView.swift
+++ nc7/ContentView.swift
@@ -15,33 +15,41 @@ struct ContentView: View {
         ZStack {
            
             VStack (spacing: 0) {
-                ForEach(Array(gameController.board.enumerated()), id: \.element) { index, array in
+
+                ForEach(gameController.board.indices, id: \.self) { x in
                     HStack (spacing: 0) {
-                        ForEach(array.indices, id: \.self) { value in
+                        ForEach(gameController.board[x].indices, id: \.self) { y in
                             VStack {
-                                Text("\(array[value])")
-                                    .foregroundStyle(array[value] == 1 ? .black : .white)
+                                Text("\(gameController.board[x][y])")
+                                    .foregroundStyle(gameController.board[x][y] == 1 ? .black : .white)
                                 
                             }
                             .frame(width: 50, height: 50)
                             .border(.black)
-                            .background(array[value] == 1 ? .white : .black)
+                            .background(gameController.board[x][y] == 1 ? .white : .black)
                             .onTapGesture {
-                                print(value)
-                                print(index)
-//                              gameController.check(x: index, y: value)
+                                print(gameController.board[x][y])
+                                if gameController.board[x][y] == 1 {
+                                    gameController.board[x][y] = 0
+                                    gameController.newBoard[x][y] = 0
+                                }
+                                
+                                else {
+                                    gameController.board[x][y] = 1
+                                    gameController.newBoard[x][y] = 1
+                                }
+                                print(gameController.board[x][y])
                             }
                         }
                     }
                 }
                 
+                Button(action: {gameController.play()}) {
+                    Text("play")
+                }
+                .padding(.top)
+                
             }
-//            VStack {
-//                Button(action: {gameController.play()}) {
-//                    Text("play")
-//                }
-//                Spacer()
-//            }
             
             
         }
@@ -51,87 +59,4 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
-}
-
-@Observable class GameController {
-    
-    var board: [[Int]] = []
-    var newBoard: [[Int]] = []
-    
-    func check(x: Int, y: Int) {
-        let middle: Int = self.board[x][y]
-        let row: Int = board.count
-        let col: Int = board[0].count
-        
-        // Em Cima
-        let p00 = self.board[(x - 1 + row) % row][(y - 1 + col) % col]
-        let p01 = self.board[(x - 1 + row) % row][(y + col) % col]
-        let p02 = self.board[(x - 1 + row) % row][(y + 1 + col) % col]
-        
-        // Mesma Linha
-        let p10 = self.board[(x + row) % row][(y - 1 + col) % col]
-        let p12 = self.board[(x + row) % row][(y + 1 + col) % col]
-        
-        // Linha De Baixo
-        let p20 = self.board[(x + 1 + row) % row][(y - 1 + col) % col]
-        let p21 = self.board[(x + 1 + row) % row][(y + col) % col]
-        let p22 = self.board[(x + 1 + row) % row][(y + 1 + col) % col]
-        
-        print("\(p00) | \(p01) | \(p02) ")
-        print("\(p10) | \(self.board[x][y]) | \(p12) ")
-        print("\(p20) | \(p21) | \(p22) ")
-        
-        let soma: Int =  p00 + p01 + p02 + p10 + p12 + p20 + p21 + p22
-        
-        if middle == 0 {
-            
-            if soma == 3 {
-                newBoard[x][y] = 1
-            }
-        }
-        else {
-            
-            if soma < 2 || soma > 3 {
-                newBoard[x][y] = 0
-            }
-            
-        }
-        
-        
-    }
-    
-    func generateBoard(size: Int) {
-        
-        for _ in 0..<size {
-            var aux: [Int] = []
-            for _ in 0..<size {
-                aux.append(Int.random(in: 0...1))
-            }
-            self.board.append(aux)
-            self.newBoard.append(aux)
-        }
-        
-    }
-    
-    func play() {
-        
-        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
-            for coluna in 0..<self.board.count {
-                for linha in 0..<self.board[coluna].count {
-                    self.check(x: linha, y: coluna)
-                }
-            }
-            self.board = self.newBoard
-        }
-        
-        
-    }
-    
-    
-    
-    init() {
-        generateBoard(size: 5)
-    }
-    
-    
 }
---
+++ nc7/GameController.swift
@@ -0,0 +1,89 @@
+//
+//  GameController.swift
+//  nc7
+//
+//  Created by Pedro Pessuto on 27/02/24.
+//
+
+import Foundation
+
+@Observable class GameController {
+    
+    var board: [[Int]] = []
+    var newBoard: [[Int]] = []
+    
+    func check(x: Int, y: Int) {
+        let middle: Int = self.board[x][y]
+        let row: Int = board.count
+        let col: Int = board[0].count
+        
+        // Em Cima
+        let p00 = self.board[(x - 1 + row) % row][(y - 1 + col) % col]
+        let p01 = self.board[(x - 1 + row) % row][(y + col) % col]
+        let p02 = self.board[(x - 1 + row) % row][(y + 1 + col) % col]
+        
+        // Mesma Linha
+        let p10 = self.board[(x + row) % row][(y - 1 + col) % col]
+        let p12 = self.board[(x + row) % row][(y + 1 + col) % col]
+        
+        // Linha De Baixo
+        let p20 = self.board[(x + 1 + row) % row][(y - 1 + col) % col]
+        let p21 = self.board[(x + 1 + row) % row][(y + col) % col]
+        let p22 = self.board[(x + 1 + row) % row][(y + 1 + col) % col]
+        
+//        print("\(p00) | \(p01) | \(p02) ")
+//        print("\(p10) | \(self.board[x][y]) | \(p12) ")
+//        print("\(p20) | \(p21) | \(p22) ")
+        
+        let sum: Int =  p00 + p01 + p02 + p10 + p12 + p20 + p21 + p22
+        
+        if middle == 0 {
+            if sum == 3 {
+                newBoard[x][y] = 1
+            }
+        }
+        else {
+            if sum < 2 || sum > 3 {
+                newBoard[x][y] = 0
+            }
+        }
+        
+    }
+    
+    func generateBoard(size: Int) {
+        
+        for _ in 0..<size {
+            var aux: [Int] = []
+            for _ in 0..<size {
+                aux.append(Int.random(in: 0...0))
+            }
+            self.board.append(aux)
+            self.newBoard.append(aux)
+        }
+        
+        
+    }
+    
+    func play() {
+        
+//        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+//            
+//        }
+        for coluna in 0..<self.board.count {
+            for linha in 0..<self.board[coluna].count {
+                self.check(x: linha, y: coluna)
+            }
+        }
+        self.board = self.newBoard
+        
+        
+    }
+    
+    
+    
+    init() {
+        generateBoard(size: 5)
+    }
+    
+    
+}
